dd69c6643312f7878881b85bb0cd9ed600f8ead8
Marcymilien <marcymilien@gmail.com>
2020-05-25 10:35:43 +0200

    Commit avant merge

---
+++ Musique test/SearchBar.swift
@@ -0,0 +1,44 @@
+//
+//  SearchBar.swift
+//  Musique test
+//
+//  Created by Maximilien Marcy on 11/05/2020.
+//  Copyright © 2020 thomas groux. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct SearchBar: UIViewRepresentable {
+    
+    @Binding var text: String
+
+    class Coordinator: NSObject, UISearchBarDelegate {
+        
+        @Binding var text: String
+        
+        init(text: Binding<String>) {
+            _text = text
+        }
+        
+        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            text = searchText
+        }
+        
+    }
+    
+    func makeCoordinator() -> SearchBar.Coordinator {
+        return Coordinator(text: $text)
+    }
+    
+    func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
+        let searchBar = UISearchBar(frame: .zero)
+        searchBar.delegate = context.coordinator
+        return searchBar
+    }
+    
+    func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<SearchBar>) {
+        uiView.text = text
+    }
+    
+}
